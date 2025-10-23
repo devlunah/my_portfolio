dd5d09bc645777ed1cef9c9ff436e3d539f97bcc
devlunah <167095503+devlunah@users.noreply.github.com>
2025-10-22 21:46:35 -0500

    corrigindo settings

--- portfolioLunah/portfolioLunah/settings.py
+++ portfolioLunah/portfolioLunah/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'landingPage',
-    'landingPage.apps.LandingpageConfig',
+    #'landingPage.apps.LandingpageConfig',
 ]
 
 MIDDLEWARE = [
