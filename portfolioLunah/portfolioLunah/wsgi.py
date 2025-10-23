54ef9ea9a82226396cca0c696588865a09175186
devlunah <lunah.silvac@gmail.com>
2025-10-23 10:38:04 -0500

    refactor: atualiza wsgi.py

--- portfolioLunah/portfolioLunah/wsgi.py
+++ portfolioLunah/portfolioLunah/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolioLunah.settings')
 
 application = get_wsgi_application()
+app = application  # ✅ necessário para Vercel
